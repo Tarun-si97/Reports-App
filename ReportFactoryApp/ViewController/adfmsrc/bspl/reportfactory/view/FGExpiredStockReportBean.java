b99323ed93864cb52a01818d1d6d2a6f4551f160
Divya Prakash Pathak <divyaprakashinbox@gmail.com>
2022-04-18 14:59:24 +0530

    FG Expired Stock
    Bean class , taskFlow and JSFF page

---
+++ ReportFactoryApp/ViewController/adfmsrc/bspl/reportfactory/view/FGExpiredStockReportBean.java
@@ -0,0 +1,39 @@
+package bspl.reportfactory.view;
+
+import javax.faces.event.ActionEvent;
+
+import model.service.AppModuleImpl;
+
+import oracle.adf.model.BindingContext;
+
+import oracle.adf.model.binding.DCIteratorBinding;
+import oracle.adf.share.logging.ADFLogger;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class FGExpiredStockReportBean {
+    public FGExpiredStockReportBean() {
+    }
+    public BindingContainer getBindings() {
+        return BindingContext.getCurrent().getCurrentBindingsEntry();
+        
+    }
+    
+    private static final ADFLogger logger = ADFLogger.createADFLogger(AppModuleImpl.class);
+    public void onClickButton(ActionEvent actionEvent) {
+        DCIteratorBinding pvIter = (DCIteratorBinding) getBindings().get("DummyVVO1Iterator");
+        String Unit = (String) pvIter.getCurrentRow().getAttribute("UnitCode");
+        
+        
+        
+            BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();  
+            OperationBinding operationBinding = bindings.getOperationBinding("callFGExpiredStock");
+            operationBinding.getParamsMap().put("LV_UNIT",Unit);
+        
+        Object result = operationBinding.execute();
+        
+        }
+        
+    
+}
